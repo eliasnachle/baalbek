3f177b041a0b007afd2baec46afdfd33d0071ced
eliasnachle <elias.nachle@bandtec.com.br>
2021-05-08 17:57:48 -0300

    Atualizando website

---
+++ Website/js/menu.js
@@ -0,0 +1,9 @@
+function open_menu(){
+    navbar.style.left = '0';
+    body.style.overflow = 'hidden';
+}
+function close_menu(){
+    navbar.style.left = '-1000px';
+    navbar.style.transition = '2s left ease-out';
+    body.style.overflow = 'unset';
+}
